c3e9669f3ea17f547140709a6a9af8994f74f49f
Adyson Vargas Medina <20220937@itla.edu.do>
2025-04-07 11:30:18 -0400

    Cors para permitir desde cualquier dominio

--- AuthUserService.Api/Program.cs
+++ AuthUserService.Api/Program.cs
@@ -41,7 +41,17 @@ builder.Services.AddAuthentication(config =>
     };
 });
 
-//Authorization
+//CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
